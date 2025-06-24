08bcdce09ba721bcaf3763351d44f2992a363351
Fotis <fotis.sofoulis@gmail.com>
2025-06-24 17:20:48 +0300

    added get_center and draw_move function and tests

--- cell.py
+++ cell.py
@@ -31,3 +31,13 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.drawline(line)
+
+    def get_center(self):
+        return ((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
+
+    def draw_move(self, to_cell, undo=False):
+        start = Point(*self.get_center())
+        end = Point(*to_cell.get_center())
+        fill_color = "gray" if undo else "red"
+        line = Line(start, end)
+        self.__win.drawline(line, fill_color)
